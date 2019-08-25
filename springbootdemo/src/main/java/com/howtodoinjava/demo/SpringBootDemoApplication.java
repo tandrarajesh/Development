ee199bb9607b2478a0fc8a093bb975bf6034055e
unknown <tandrarajesh28@gmail.com>
2019-08-25 23:28:59 +0530

    craeting a springboot project

---
+++ springbootdemo/src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+}
